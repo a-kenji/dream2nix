abd78f16779c012f511e0b7c2ddab17b54669d41
a-kenji <aks.kenji@protonmail.com>
2022-07-22 15:56:28 +0200

    fixup!

--- examples/debian-control/flake.nix
+++ examples/debian-control/flake.nix
@@ -9,21 +9,20 @@
     self,
     dream2nix,
     src,
-  } @ inp:
-    (dream2nix.lib.makeFlakeOutputs {
-      systems = ["x86_64-linux"];
-      config.projectRoot = ./.;
-      source = src;
-      # settings = [
-      #   {
-      #     # optionally define python version
-      #     subsystemInfo.pythonAttr = "python38";
-      #     # # optionally define extra setup requirements;
-      #     subsystemInfo.extraSetupDeps = ["cython > 0.29"];
-      #   }
-      # ];
-    });
-    # // {
-    #   checks.x86_64-linux.aiohttp = self.packages.x86_64-linux.main;
-    # };
+  } @ inp: (dream2nix.lib.makeFlakeOutputs {
+    systems = ["x86_64-linux"];
+    config.projectRoot = ./.;
+    source = src;
+    # settings = [
+    #   {
+    #     # optionally define python version
+    #     subsystemInfo.pythonAttr = "python38";
+    #     # # optionally define extra setup requirements;
+    #     subsystemInfo.extraSetupDeps = ["cython > 0.29"];
+    #   }
+    # ];
+  });
+  # // {
+  #   checks.x86_64-linux.aiohttp = self.packages.x86_64-linux.main;
+  # };
 }
